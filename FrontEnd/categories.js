911aba2073efda2321c983ad9d38c764c70c7abb
Gaudou Noémie <gaudou.noemie@gmail.com>
2024-09-27 12:22:18 +0200

    Mise en fonction des boutons filtres

--- FrontEnd/categories.js
+++ FrontEnd/categories.js
@@ -1,35 +1,52 @@
 
+
 try{
-    const reponse = await fetch("http://localhost:5678/api/categories");    //on récupère les informations de l'API dans "reponse"
-    const categories = await reponse.json();                               //on convertie les informations en format json
-    
+    const reponse = await fetch("http://localhost:5678/api/categories");   //On récupère les informations de l'API dans "reponse"
+    const categories = await reponse.json();                               //On convertie les informations en format json
+
+    const categoriesDiv = document.querySelector(".categoriesDiv");       // Sélection de la div où les boutons seront ajoutés
 
 
-    categories.forEach((element) => {                                 // pour categories on crée une boucles pour afficher chaque éléments
-        const button = document.createElement("button");                  //on crée un bouton
-        button.classList.add("filter-btn")                                //ce bouton aura une class nommé "filter-btn"
-        button.innerHTML = element.name                               //le texte à l'interieur du bouton sera le nom données dans l'API
-        button.dataset.id = element.id                                  //l'id sera celle donné dans l'API
+        
+        const allButton = document.createElement("button");                // Créer le bouton "Tous"
+        allButton.classList.add("filter-btn");                             // Bouton "Tous" aura une classe nommé "filter-btn"
+        allButton.innerHTML = "Tous";                                      // Le texte du bouton "Tous"
+        allButton.dataset.id = "btn-Tous";                                 // On lui attribut une id nommée "btn-Tous"
+
+        allButton.addEventListener("click", function(){                    // Fonction d'écoute au click
+            document.querySelector(".gallery").innerText = "";             // On vide le text
+            generergallery(window.gallery);                                // On génère la gallery
+        });
+
+        categoriesDiv.appendChild(allButton);                              // Ajouter le bouton "Tous" à la div des catégories
+
+    categories.forEach((element) => {                                      // Pour categories on crée une boucles pour afficher chaque éléments
+        const button = document.createElement("button");                   // On crée un bouton
+        button.classList.add("filter-btn")                                 // Ce bouton aura une class nommé "filter-btn"
+        button.innerHTML = element.name                                    // le texte à l'interieur du bouton sera le nom données dans l'API
+        button.dataset.id = element.id                                     // l'id sera celle donné dans l'API
 
       
-        button.addEventListener("click", function() {
-            document.querySelector(".gallery").innerText = ""
-        const categoryIdToFilter = this.dataset.id;
-        const filteredGallery = gallery.filter(function(item) {
-            return item.categoryElement === parseInt(categoryIdToFilter);  // Compare les IDs
-        
+        button.addEventListener("click", function() {                      // Fonction d'écoute au click
+        document.querySelector(".gallery").innerText = ""                  // On vide le text 
+        const categoryIdToFilter = this.dataset.id;                        // On stock id selectionnée
+        const filteredGallery = window.gallery.filter(function(item) {     // Contient la nouvelle version filtré
+            return item.categoryId === parseInt(categoryIdToFilter);       // Compare les IDs
+       
         });
+    console.log("Catégorie filtrée :", categoryIdToFilter);           // On vérifie les données dans la console
+    console.log("Galerie filtrée :", filteredGallery);                    // On vérifie les données dans la console
         
-    document.querySelector(".gallery").innerHTML = ""
-    generergallery(filteredGallery);
+   
+    generergallery(filteredGallery);                                      // On génère la gallery filtré
     });   
   
-   categoriesDiv.appendChild(button);                                //bouton devient l'enfant de categoriesDiv
+        categoriesDiv.appendChild(button);                                      // Bouton devient l'enfant de categoriesDiv
 });
 
 } catch (error){
-categoriesDiv.innerText = "Erreur lors de la récupération des données catégories";
-console.log(error);
+categoriesDiv.innerText = "Erreur lors de la récupération des données catégories";     // On indique l'erreur à l'utilisateur
+console.log(error);                                                        // On indique une erreur dans la console
 }
 
 
--- FrontEnd/projet.js
+++ FrontEnd/projet.js
@@ -1,37 +1,46 @@
-import "./categories.js"
+import "./categories.js"                                         // On iporte le fichier categories.js dans projey.js
 
-function generergallery(gallery) {                               // on crée une fonction que va generer la gallery
-    for (let i=0; i < gallery.length; i++) {                     // on crée une boucle qui va s'executer le nombre de fois qu'il y a d'objet dans gallery
+window.gallery = [];                                             // On indique que gallery est une variable globale
+window.generergallery = generergallery;                          // On rend la fonction generergallery globale
 
-    const projet = gallery[i];                                      //projet va contenir la valeur de gallery(objet js)
+function generergallery(gallery) {                               // On crée une fonction que va generer la gallery
+    for (let i=0; i < gallery.length; i++) {                     // On crée une boucle qui va s'executer le nombre de fois qu'il y a d'objet dans gallery
 
- const divGallery = document.querySelector(".gallery");          //on selection la div  nommé "gallery"
- const projetElement = document.createElement("article");        // on crée un article qui sera le parent de chaque projet photo
- projetElement.dataset.id = gallery[i].id                        // on recupère id de l'élement
- const imageElement = document.createElement("img");             // on crée une image
- imageElement.src = projet.imageUrl;                             // on recherche la source de l'image et son chemin 
- const titleElement = document.createElement("figcaption");      //on crée figcaption pour l'image
- titleElement.innerText = projet.title;                          // ajout le nom du titre de l'image
- const categoryElement = gallery[i].categoryId;                   // ajout de la categorie de l'image
- const userElement = gallery[i].userId;                           // ajout de userId de l'image
+    const projet = gallery[i];                                   // Projet va contenir la valeur de gallery(objet js)
+
+ const divGallery = document.querySelector(".gallery");          // On selection la div  nommé "gallery"      
+ const projetElement = document.createElement("article");        // On crée un article qui sera le parent de chaque projet photo
+ projetElement.dataset.id = gallery[i].id                        // On recupère id de l'élement
+ const imageElement = document.createElement("img");             // On crée une image
+ imageElement.src = projet.imageUrl;                             // On recherche la source de l'image et son chemin 
+ const titleElement = document.createElement("figcaption");      // On crée figcaption pour l'image
+ titleElement.innerText = projet.title;                          // Ajout le nom du titre de l'image
+ const categoryElement = projet.category;                        // Ajout de la categorie de l'image
+ const userElement = projet.userId;                              // Ajout de userId de l'image
 
 
 
 
 // ajout des éléments enfant
-divGallery.appendChild(projetElement);                            // projetElement devient l'enfant de divGallery
-projetElement.appendChild(imageElement);                          //imageElement devient l'enfant de projetElement
-projetElement.appendChild(titleElement);                          //titleElement devient l'enfant de projetElement
+divGallery.appendChild(projetElement);                            // ProjetElement devient l'enfant de divGallery
+projetElement.appendChild(imageElement);                          // ImageElement devient l'enfant de projetElement
+projetElement.appendChild(titleElement);                          // TitleElement devient l'enfant de projetElement
  }
 }
 
 
-try{                                                               //on lui demande de récuperer les informations de l'API
-    const reponse = await fetch("http://localhost:5678/api/works"); //on recupère les donnees de l'API dans "reponse"
-    const gallery = await reponse.json();                           //on convertie la réponse en format json
-    generergallery(gallery);                                        //appel la fonction avec les données du tableau gallery
+try{                                                               // On lui demande de récuperer les informations de l'API
+    const reponse = await fetch("http://localhost:5678/api/works"); // On recupère les donnees de l'API dans "reponse"
+    const gallery = await reponse.json();                           // On convertie la réponse en format json
+    window.gallery = gallery;                                      // Permet d'accéder globalement a gallery dans le script
 
-} catch(error){                                                    // si il y à une erreur de récupération de données
-     divGallery = document.querySelector(".gallery")          // on selection l'endroit ou il peut y avoir une erreur
+    generergallery(gallery);                                       // Appel la fonction avec les données du tableau gallery
+    
+    console.log("Données de la galerie :", window.gallery);        // Vérification de la réponse
+    console.log("Réponse de l'API :", reponse);                    // Vérification de la réponse
+    console.log("Données JSON :", gallery);                        // Vérification de la réponse
+
+} catch(error){                                                    // Si il y à une erreur de récupération de données
+     divGallery = document.querySelector(".gallery")               // On selection l'endroit ou il peut y avoir une erreur
     divGallery.innerText = "Les données de l'API n'ont pu être récupérées."  //On indique le problème dans divGallery
 }
