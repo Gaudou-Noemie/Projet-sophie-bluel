909751a2d8634ccc52cd6853fb79a7ef74227979
Gaudou Noémie <gaudou.noemie@gmail.com>
2024-10-22 19:31:58 +0200

    Vérification de la fin du projet

--- FrontEnd/functionModalAdd.js
+++ FrontEnd/functionModalAdd.js
@@ -9,7 +9,7 @@ async function fetchCategory(categoryInput){
         const categories = await rep.json();
         console.log("Catégories récupérées:", categories);
        
-        categories.forEach((category) => {                                     // On fait une boucle pour récupérer les categorie
+        categories.forEach((category) => {                                 // On fait une boucle pour récupérer les categorie
             const option = document.createElement("option");               // On crée une option
             option.value = category.id;                                    // La valeur sera id de catégorie
             option.textContent = category.name;                            // Le texte sera le name de catégorie
@@ -22,7 +22,7 @@ async function fetchCategory(categoryInput){
 
  function createErrorMessage(message) {
     const errorMessage = document.createElement("div");
-    errorMessage.style.backgroundColor = "#FF6F61"; // Couleur rouge pour l'erreur
+    errorMessage.style.backgroundColor = "#FF6F61";             // Couleur rouge pour l'erreur
     errorMessage.style.color = "white";
     errorMessage.style.padding = "10px";
     errorMessage.style.margin = "10px 0";
@@ -36,7 +36,7 @@ async function fetchCategory(categoryInput){
 
 function createSuccessMessage(message) {
     const successMessage = document.createElement("div");
-    successMessage.style.backgroundColor = "#4CAF50";  // Couleur verte pour le succès
+    successMessage.style.backgroundColor = "#4CAF50";          // Couleur verte pour le succès
     successMessage.style.color = "white";
     successMessage.style.padding = "15px";
     successMessage.style.margin = "10px 0";
@@ -51,7 +51,7 @@ function createSuccessMessage(message) {
 function showSuccessMessage(message){
     const successMessage = createSuccessMessage(message);
     const main = document.querySelector("main");
-    main.prepend(successMessage);  // Ajouter le message en haut du formulaire
+    main.prepend(successMessage);                         // Ajouter le message en haut du formulaire
 
          // Cacher le message après quelques secondes
          setTimeout(() => {
